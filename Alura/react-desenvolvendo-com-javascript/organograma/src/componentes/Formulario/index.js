66ee9f66f79de8edb90513d0b7b5bfc3da9832c3
leosouza-dev <leosouzadev1@gmail.com>
2023-11-21 14:54:14 -0300

    formulario submetendo novo colaborador

--- Alura/react-desenvolvendo-com-javascript/organograma/src/componentes/Formulario/index.js
+++ Alura/react-desenvolvendo-com-javascript/organograma/src/componentes/Formulario/index.js
@@ -4,7 +4,8 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
+const Formulario = (props) => {
+    // const {aoColaboradorCadastrado} = props;
     const times = ['Programação', 'Front-End', 'Data Science', 'Devops', 'UX e Design', 'Mobile', 'Inovação e Gestão']
 
     const [nome, setNome] = useState('');
@@ -15,6 +16,7 @@ const Formulario = () => {
     const aoSalvar = (event) => {
         event.preventDefault();
         console.log('Foi submetido', nome, cargo, imagem, time);
+        props.aoColaboradorCadastrado({nome, cargo, imagem, time})
     }
 
     return (
--- Alura/react-desenvolvendo-com-javascript/organograma/src/componentes/ListaSuspensa/index.js
+++ Alura/react-desenvolvendo-com-javascript/organograma/src/componentes/ListaSuspensa/index.js
@@ -1,11 +1,13 @@
 import './ListaSuspensa.css'
 
 const ListaSuspensa = (props) => {
+    console.log('teste', props.itens[0])
     return (
         <div className='lista-suspensa'>
             <label>{props.label}</label>
             <select 
                 value={props.valor} 
+                defaultValue={props.itens[0]}
                 onChange={evento => props.aoAlterado(evento.target.value)}>
                     {props.itens.map(item => <option key={item}>{item}</option>)}
             </select>
